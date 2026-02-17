c263e623c9ae19d60135b9e8caf46b75b8e8bc89
mehaklaha <mehaklaha@gmail.com>
2026-02-17 09:44:02 +0530

    Change CORS settings to allow all origins
    Updated CORS middleware to allow all origins.

--- main.py
+++ main.py
@@ -13,11 +13,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3004",
-        "https://skipthefires.netlify.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
